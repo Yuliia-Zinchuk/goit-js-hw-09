775eed9ab6747297ae1eb7a5e2290950560aecb0
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-09-06 17:17:50 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+buttonStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    buttonStart.setAttribute('disabled', true);
+    buttonStop.removeAttribute('disabled');
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStart.removeAttribute('disabled');
+  buttonStop.setAttribute('disabled', true);
+});
